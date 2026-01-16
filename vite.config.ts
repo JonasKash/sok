757dae524573834edba58b0c76adddc19eab3e4d
Jonas <agencialobuzlancamentos@gmail.com>
2026-01-16 03:05:24 -0300

    alteração no vit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // @ts-expect-error process.cwd() is available in the Node.js environment where Vite runs
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
